965dc19dde3fd3f13cb7e7c489b1c4f8b95667e4
Anton Duppils <dat13adu@student.lth.se>
2018-11-27 15:20:50 +0100

    deleted all my work, but now most is back, INPUT ORDER and MIN DOMAIN works, not optimal..

--- lab3/Golomb.java
+++ lab3/Golomb.java
@@ -68,116 +68,117 @@ import org.jacop.search.SelectChoicePoint;
 
 public class Golomb {
 
-    Store store;
+	Store store;
 
-    /**
-     * It specifies the number of marks (number of natural numbers in 
-     * the sequence).
-     */
-    public int noMarks = 10;
+	/**
+	 * It specifies the number of marks (number of natural numbers in 
+	 * the sequence).
+	 */
+	public int noMarks = 10;
 
-    /**
-     * It specifies the upper bound of the optimal solution.
-     */
-    public int bound = -1;
+	/**
+	 * It specifies the upper bound of the optimal solution.
+	 */
+	public int bound = -1;
 
-	
-    /**
-     * It contains all differences between all possible pairs of marks.
-     */
-    public ArrayList<IntVar> subs = new ArrayList<IntVar>();
-	
-    /**
-     * It executes the program which computes the optimal Golomb ruler. 
-     * 
-     * @param args the first argument specifies the number of marks, the second argument specifies the upper bound of the optimal solution.
-     */
-    public static void main(String args[]) {
-		
-	Golomb example = new Golomb();
-		
-	example.model();
 
-    }			
+	/**
+	 * It contains all differences between all possible pairs of marks.
+	 */
+	public ArrayList<IntVar> subs = new ArrayList<IntVar>();
 
-    public void model() {
+	/**
+	 * It executes the program which computes the optimal Golomb ruler. 
+	 * 
+	 * @param args the first argument specifies the number of marks, the second argument specifies the upper bound of the optimal solution.
+	 */
+	public static void main(String args[]) {
+		Golomb example = new Golomb();
+		//run all search types
+		example.model(0);
+		System.out.println("-----------------------------");
+		example.model(1);
 
-	System.out.println("Program to solve Golomb mark problem - length "
-			   + noMarks);
+	}			
 
-	store = new Store();
-	ArrayList<IntVar> vars = new ArrayList<IntVar>();
-		
-	IntVar numbers[] = new IntVar[noMarks];
+	public void model(int selection) {
 
-	for (int i = 0; i < numbers.length; i++) {
-	    // Create FDV for each natural number
-	    numbers[i] = new IntVar(store, "n" + new Integer(i), (i + 1) * i / 2,
-				    noMarks * noMarks);
+		System.out.println("Program to solve Golomb mark problem - length "
+				+ noMarks);
 
-	    // Impose constraints that each consequtive number
-	    // is larger than the previous one
-	    // Golomb ruler is an ordered sequence of numbers
-	    if (i > 0)
-		store.impose(new XltY(numbers[i - 1], numbers[i]));
-	    else
-		store.impose(new XeqC(numbers[0], 0));
+		store = new Store();
+		ArrayList<IntVar> vars = new ArrayList<IntVar>();
+
+		IntVar numbers[] = new IntVar[noMarks];
+
+		for (int i = 0; i < numbers.length; i++) {
+			// Create FDV for each natural number
+			numbers[i] = new IntVar(store, "n" + new Integer(i), (i + 1) * i / 2,
+					noMarks * noMarks);
+
+			// Impose constraints that each consequtive number
+			// is larger than the previous one
+			// Golomb ruler is an ordered sequence of numbers
+			if (i > 0)
+				store.impose(new XltY(numbers[i - 1], numbers[i]));
+			else
+				store.impose(new XeqC(numbers[0], 0));
+		}
+
+		for (IntVar v : numbers)
+			vars.add(v);
+
+		if (bound > -1)
+			store.impose(new XlteqC(numbers[noMarks - 1], bound));
+
+		// ArrayList contains all differences
+		subs = new ArrayList<IntVar>();
+
+		for (int i = 1; i < numbers.length; i++) {
+
+			// for (int j = i - 1; j >= 0; j--) {
+			for (int j = 0; j < i; j++) {
+				// Create FDV for a difference between ith and jth number
+				IntVar sub = new IntVar(store, "c" + i + "_" + j, (i - j)
+						* (i - j + 1) / 2, noMarks * noMarks);
+
+				subs.add(sub);
+
+				// sub + jth = ith since sub = ith - jth
+				// Add constraint so the above relationship holds
+				//			store.imposePropagators(new XplusYeqZ(sub, numbers[j], numbers[i]));
+				store.impose(new XplusYeqZ(sub, numbers[j], numbers[i]));
+
+			}
+			}
+
+		int index = 0;
+		for (int i = 1; i < noMarks; i++)
+			for (int j = 0; j < i; j++)
+				//				store.imposePropagators(new XplusClteqZ(subs.get(index++), (noMarks - 1 - i + j)
+				//						* (noMarks - i + j) / 2, numbers[noMarks - 1]));
+				store.impose(new XplusClteqZ(subs.get(index++), (noMarks - 1 - i + j)
+							* (noMarks - i + j) / 2, numbers[noMarks - 1]));
+
+		// symmetry breaking constraint
+		// important constraint to reduce search space since
+		// interested in proving the optimality
+		store.impose(new XltY(subs.get(0), subs.get(subs.size() - 1)));
+
+		// All differences have to have unique values
+		store.impose(new Alldiff(subs), 1);
+
+		IntVar cost = numbers[numbers.length - 1];
+
+		SimpleDFS search = new SimpleDFS(store, selection);
+		search.setVariablesToReport(numbers);
+		search.setCostVariable(cost);
+
+		boolean result = search.label(numbers);
+
+		System.out.println (result);
+
+		}
+
+
 	}
-
-	for (IntVar v : numbers)
-	    vars.add(v);
-		
-	if (bound > -1)
-	    store.impose(new XlteqC(numbers[noMarks - 1], bound));
-
-	// ArrayList contains all differences
-	subs = new ArrayList<IntVar>();
-
-	for (int i = 1; i < numbers.length; i++) {
-
-	    // for (int j = i - 1; j >= 0; j--) {
-	    for (int j = 0; j < i; j++) {
-		// Create FDV for a difference between ith and jth number
-		IntVar sub = new IntVar(store, "c" + i + "_" + j, (i - j)
-					* (i - j + 1) / 2, noMarks * noMarks);
-
-		subs.add(sub);
-
-		// sub + jth = ith since sub = ith - jth
-		// Add constraint so the above relationship holds
-		//			store.imposePropagators(new XplusYeqZ(sub, numbers[j], numbers[i]));
-		store.impose(new XplusYeqZ(sub, numbers[j], numbers[i]));
-
-	    }
-	}
-
-	int index = 0;
-	for (int i = 1; i < noMarks; i++)
-	    for (int j = 0; j < i; j++)
-		//				store.imposePropagators(new XplusClteqZ(subs.get(index++), (noMarks - 1 - i + j)
-		//						* (noMarks - i + j) / 2, numbers[noMarks - 1]));
-		store.impose(new XplusClteqZ(subs.get(index++), (noMarks - 1 - i + j)
-					     * (noMarks - i + j) / 2, numbers[noMarks - 1]));
-
-	// symmetry breaking constraint
-	// important constraint to reduce search space since
-	// interested in proving the optimality
-	store.impose(new XltY(subs.get(0), subs.get(subs.size() - 1)));
-
-	// All differences have to have unique values
-	store.impose(new Alldiff(subs), 1);
-
-	IntVar cost = numbers[numbers.length - 1];
-
-	SimpleDFS search = new SimpleDFS(store);
-	search.setVariablesToReport(numbers);
-	search.setCostVariable(cost);
-
-	boolean result = search.label(numbers);
-	
-	System.out.println (result);
-
-    }
-	
-	
-}
